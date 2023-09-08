de5ac9f38c2f777cff3a95a7b4b24255945ebec0
tooooo1 <wer4272@nate.com>
2023-09-08 13:49:39 +0900

    refactor: checkWebPSupport to use Promise

--- packages/image-preloader/src/__test__/preloadImages.test.ts
+++ packages/image-preloader/src/__test__/preloadImages.test.ts
@@ -2,9 +2,8 @@ import { describe, expect, test } from "vitest";
 import { checkWebPSupport } from "../index";
 
 describe("WebP Format Support", () => {
-  test("checkWebPSupport should return a boolean", () => {
-    checkWebPSupport((isSupported) => {
-      expect(typeof isSupported).toBe("boolean");
-    });
+  test("checkWebPSupport should return a boolean", async () => {
+    const isSupported = await checkWebPSupport();
+    expect(typeof isSupported).toBe("boolean");
   });
 });
--- packages/image-preloader/src/index.ts
+++ packages/image-preloader/src/index.ts
@@ -1,17 +1,19 @@
 // https://developers.google.com/speed/webp/faq?hl=ko#in_your_own_javascript
-function checkWebPSupport(callback: (result: boolean) => void) {
-  const testImage = "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
-  const img = new Image();
+function checkWebPSupport(): Promise<boolean> {
+  return new Promise((resolve) => {
+    const testImage = "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
+    const img = new Image();
 
-  img.onload = () => {
-    callback(img.width > 0 && img.height > 0);
-  };
+    img.onload = () => {
+      resolve(img.width > 0 && img.height > 0);
+    };
 
-  img.onerror = () => {
-    callback(false);
-  };
+    img.onerror = () => {
+      resolve(false);
+    };
 
-  img.src = "data:image/webp;base64," + testImage;
+    img.src = "data:image/webp;base64," + testImage;
+  });
 }
 
 interface ImageSources {
@@ -19,14 +21,14 @@ interface ImageSources {
   webp?: string;
 }
 
-function preloadImages(images: ImageSources[]) {
-  checkWebPSupport((supportsWebp) => {
-    images.forEach((image) => {
-      const src = supportsWebp && image.webp ? image.webp : image.defaultSrc;
-      const img = new Image();
-      img.src = src;
-    });
-  });
+async function preloadImages(images: ImageSources[]) {
+  const supportsWebp = await checkWebPSupport();
+
+  for (const image of images) {
+    const src = supportsWebp && image.webp ? image.webp : image.defaultSrc;
+    const img = new Image();
+    img.src = src;
+  }
 }
 
 export { checkWebPSupport, preloadImages };
