b40f3183624c1945bdb4d493c9ec00ccc99145ce
tooooo1 <wer4272@nate.com>
2023-09-08 13:43:12 +0900

    feat: Simplify test suite to focus solely on WebP format support

--- packages/image-preloader/src/__test__/preloadImages.test.ts
+++ packages/image-preloader/src/__test__/preloadImages.test.ts
@@ -1,60 +1,10 @@
-import { afterEach, beforeAll, describe, expect, test } from "vitest";
-import { preloadImages, supportsImageFormat } from "../index";
+import { describe, expect, test } from "vitest";
+import { checkWebPSupport } from "../index";
 
-describe("Image Format Support", () => {
-  test("supportsImageFormat should return true for webp if supported", () => {
-    const isSupported = supportsImageFormat("webp");
-    expect(typeof isSupported).toBe("boolean");
-  });
-
-  test("supportsImageFormat should return true for avif if supported", () => {
-    const isSupported = supportsImageFormat("avif");
-    expect(typeof isSupported).toBe("boolean");
-  });
-
-  test("supportsImageFormat should return false for unsupported formats", () => {
-    const isSupported = supportsImageFormat("unsupported-format");
-    expect(isSupported).toBe(false);
-  });
-
-  test("supportsImageFormat should return false for non-string formats", () => {
-    // @ts-ignore
-    const isSupported = supportsImageFormat(12345);
-    expect(isSupported).toBe(false);
-  });
-});
-
-describe("Preload Images", () => {
-  let mockImageSrc: string | undefined;
-
-  beforeAll(() => {
-    Object.defineProperty(global.Image.prototype, "src", {
-      set(src) {
-        mockImageSrc = src;
-      },
+describe("WebP Format Support", () => {
+  test("checkWebPSupport should return a boolean", () => {
+    checkWebPSupport((isSupported) => {
+      expect(typeof isSupported).toBe("boolean");
     });
-  });
-
-  afterEach(() => {
-    mockImageSrc = undefined;
-  });
-
-  test("avif support, preloadImages should preload the provided images", () => {
-    const mockImages = [
-      {
-        png: "./images/test.png",
-        webp: "./images/test.webp",
-        avif: "./images/test.avif",
-      },
-    ];
-
-    preloadImages(mockImages);
-    expect(mockImageSrc).toBe("./images/test.avif");
-  });
-
-  test("preloadImages should handle empty image array", () => {
-    const mockImages = [];
-    preloadImages(mockImages);
-    expect(mockImageSrc).toBeUndefined();
   });
 });
