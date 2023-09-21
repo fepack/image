899327189659a9edd2d3edb0c0a9b298971f61ae
tooooo1 <wer4272@nate.com>
2023-09-21 15:54:36 +0900

    feat: test for the 'load' function

---
+++ packages/image/src/__test__/load.test.ts
@@ -0,0 +1,49 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { type ImageSource, load } from "../load";
+
+class MockImage {
+  public src = "";
+
+  public setAttribute(name: string, value: string) {
+    this[name] = value;
+  }
+
+  public constructor() {
+    MockImage.lastInstance = this;
+  }
+
+  public static lastInstance: MockImage;
+}
+
+describe("load function", () => {
+  const originalImage = window.Image;
+
+  beforeAll(() => {
+    window.Image = MockImage as unknown as typeof Image;
+  });
+
+  afterAll(() => {
+    window.Image = originalImage;
+  });
+
+  it("should load defaultSrc when webpSrc is not provided", () => {
+    const testImage: ImageSource = {
+      defaultSrc: "./images/test.png",
+    };
+
+    load([testImage]);
+
+    expect(MockImage.lastInstance.src).toBe("./images/test.png");
+  });
+
+  it("should load webpSrc when provided", () => {
+    const testImage: ImageSource = {
+      defaultSrc: "./images/test.png",
+      webpSrc: "./images/test.webp",
+    };
+
+    load([testImage]);
+
+    expect(MockImage.lastInstance.src).toBe("./images/test.webp");
+  });
+});
