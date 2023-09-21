b1ab2c3dbbd354e3e0aebc55912cc7fc3f6e2cc3
tommy(Chung-il) <jungchungil7@gmail.com>
2023-09-22 00:45:12 +0900

    Update packages/image/src/__test__/load.test.ts
    Co-authored-by: Jonghyeon Ko <manudeli.ko@gmail.com>

--- packages/image/src/__test__/load.test.ts
+++ packages/image/src/__test__/load.test.ts
@@ -1,5 +1,5 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { type ImageSource, load } from "../load";
+import { type ImageSource, load } from "..";
 
 class MockImage {
   public src = "";
