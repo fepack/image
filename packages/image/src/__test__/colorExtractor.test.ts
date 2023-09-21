00981e6bb81ac05e7655e5f5e7a01194b8fa611f
tooooo1 <wer4272@nate.com>
2023-09-22 00:59:20 +0900

    chore: remove path from slash('/')

--- packages/image/src/__test__/colorExtractor.test.ts
+++ packages/image/src/__test__/colorExtractor.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { colorExtractor } from "../";
+import { colorExtractor } from "..";
 
 describe("colorExtractor function", () => {
   it("should return an array of colors", async () => {
