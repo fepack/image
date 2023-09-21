a8acffb7a877f72d5e0eea998c88ea3162e99512
tooooo1 <wer4272@nate.com>
2023-09-22 00:42:24 +0900

    feat: color extractor

--- packages/image/src/index.ts
+++ packages/image/src/index.ts
@@ -1,3 +1,4 @@
 export { checkWebPSupport } from "./checkWebPSupport";
+export { colorExtractor } from "./colorExtractor";
 export { detect } from "./detect";
 export { load, type ImageSource } from "./load";
