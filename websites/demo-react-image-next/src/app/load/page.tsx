2cec97e22b781b137aedeb9ae40988f5698d9b94
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-25 03:35:25 +0900

    chore: update

--- websites/demo-react-image-next/src/app/load/page.tsx
+++ websites/demo-react-image-next/src/app/load/page.tsx
@@ -5,18 +5,18 @@ import { Suspense } from "@suspensive/react";
 import Link from "next/link";
 import { Area, Spinner } from "~/components/uis";
 
-const BoundaryPage = () => (
-  <div>
-    <Link href="/">to home</Link>
-    <Area title="Load">
-      <Suspense.CSROnly fallback={<Spinner />}>
-        <Load src="https://ik.imagekit.io/ikmedia/women-dress-2.jpg">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          {(loaded) => <img src={loaded.src} />}
-        </Load>
-      </Suspense.CSROnly>
-    </Area>
-  </div>
-);
-
-export default BoundaryPage;
+export default function Page() {
+  return (
+    <div>
+      <Link href="/">to home</Link>
+      <Area title="Load">
+        <Suspense.CSROnly fallback={<Spinner />}>
+          <Load src="https://ik.imagekit.io/ikmedia/women-dress-2.jpg">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            {(loaded) => <img src={loaded.src} />}
+          </Load>
+        </Suspense.CSROnly>
+      </Area>
+    </div>
+  );
+}
