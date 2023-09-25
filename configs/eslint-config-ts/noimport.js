988dd36154dc434a71b20c0d1853956313a83fd3
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-26 05:37:58 +0900

    ci(eslint): add eslint-plugin-jsdoc

--- configs/eslint-config-ts/noimport.js
+++ configs/eslint-config-ts/noimport.js
@@ -3,12 +3,16 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   extends: [
+    "plugin:jsdoc/recommended-typescript",
     "@fepack/eslint-config-js/noimport",
     "plugin:@typescript-eslint/recommended",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ["jsdoc", "@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-empty-function": ["off"],
+    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    "jsdoc/require-returns": "off",
+    "jsdoc/require-description": "warn",
+    "jsdoc/check-param-names": "error",
   },
 };
