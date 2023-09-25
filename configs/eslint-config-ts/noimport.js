7fd61112fdf705e20753fe66e649c97519595aab
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-26 06:23:44 +0900

    ci(eslint): add eslint-plugin-jsdoc (#53)
    ## Summary
    
    <!-- Please summarize your changes. -->
    
    <!-- Please link to any applicable information (forum posts, bug
    reports, etc.). -->
    
    I added
    [eslint-plugin-jsdoc](https://github.com/gajus/eslint-plugin-jsdoc)
    This will help us make conventional and sustainable jsdoc comment
    
    ## Checks
    
    <!-- For completed items, change [ ] to [x]. -->
    
    <!-- If you leave this checklist empty, your PR will very likely be
    closed. -->
    
    Please check the following:
    
    - [x] I have written documents and tests, if needed.

--- configs/eslint-config-ts/noimport.js
+++ configs/eslint-config-ts/noimport.js
@@ -3,12 +3,17 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   extends: [
+    "plugin:jsdoc/recommended-typescript",
     "@fepack/eslint-config-js/noimport",
     "plugin:@typescript-eslint/recommended",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ["jsdoc", "@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-empty-function": ["off"],
+    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    "jsdoc/require-description": "warn",
+    "jsdoc/require-returns": "off",
+    "jsdoc/require-jsdoc": "off",
+    "jsdoc/check-param-names": "error",
   },
 };
