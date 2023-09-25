259c3278ba2f97d5fc6884fd6b3f7993c7916177
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-25 21:32:06 +0900

    chore: update

--- packages/react-image/tsup.config.ts
+++ packages/react-image/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   banner: { js: '"use client"' },
   format: ["cjs", "esm"],
-  entry: ["{src,src/experimental}/*.{ts,tsx}", "!**/*.{spec,test,test-d}.*"],
+  entry: ["src/*.{ts,tsx}", "!**/*.{spec,test}.*"],
   sourcemap: true,
   dts: true,
   splitting: false,
