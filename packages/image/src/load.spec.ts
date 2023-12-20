e2d4fc4ca6004c71d36bf417ce5b3e90d3b4e276
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-12-21 01:26:16 +0900

    chore: update

--- packages/image/src/load.spec.ts
+++ packages/image/src/load.spec.ts
@@ -4,6 +4,6 @@ import { load } from ".";
 describe("load", () => {
   it("should load image by src", async () => {
     const image = await load("src/images/test.png");
-    expect(image.src).toBe("http://localhost:5174/src/images/test.png");
+    expect(image.src).toBe("http://localhost:5173/src/images/test.png");
   });
 });
